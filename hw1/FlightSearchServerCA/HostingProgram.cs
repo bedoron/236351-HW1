846ebb0300051dead8ff9428c9ab0547d9db1f43
sitamarn <sitamarn@t2.technion.ac.il>
2013-11-06 22:45:40 +0200

    Intial Commit
    Adding initial working template

---
+++ hw1/FlightSearchServerCA/HostingProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace FlightSearchServerCA
+{
+    public class HostingProgram
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(
+                typeof(ClientQueryService), new Uri("http://localhost:50000/Services")))
+            {
+                host.Open();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ hw1/FlightSearchServerCA/IClientQueryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace FlightSearchServerCA
+{
+    [ServiceContract]
+    public interface IClientQueryService
+    {
+        [WebGet(UriTemplate = "flight?src={src}&dst={dst}&date={date}")]
+        [OperationContract]
+        Flights GetFlights(string src, string dst, string date);
+
+        [WebInvoke(Method = "POST", UriTemplate = "seller/{seller}")]
+        [OperationContract]
+        int MakeReservation(string seller, ReservationRequest request);
+
+        [WebInvoke(Method = "DELETE", UriTemplate = "seller/{seller}/{reservationID}")]
+        [OperationContract]
+        void CancelReservation(string seller, string reservationID);
+
+    }
+}
