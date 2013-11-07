a60cb8825e95571b5cc32d5341738f26191751f7
sitamarn <sitamarn@t2.technion.ac.il>
2013-11-07 14:13:02 +0200

    SLN fix

---
+++ hw1/TicketSellingServer/HostingProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace TicketSellingServer
+{
+    public class HostingProgram
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(
+                typeof(TicketSellingQueryService), new Uri("http://localhost:50001/Services")))
+            {
+                
+                host.Open();
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- hw1/TicketSellingServer/TicketSellingQueryService.cs
+++ hw1/TicketSellingServer/TicketSellingQueryService.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Web;
+using System.IO;
 
 namespace TicketSellingServer
 {
 
     public class TicketSellingQueryService : ITicketSellingQueryService
     {
+        private Flights flights;
+
+        public void getFlights(string filePath){
+            flights = new Flights();
+            StreamReader reader = new StreamReader(filePath);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                Flight flight = new Flight();
+                string[] members = line.Split(' ');
+                flight.flightNumber = members[0];
+                flight.src = members[1];
+                flight.dst = members[2];
+                flight.date = DateTime.Parse(members[3]);
+                flight.seats = Convert.ToInt32(members[4]);
+                flight.price = Convert.ToInt32(members[5]);
+                flights.Add(flight);
+                line = reader.ReadLine();
+            }
+ 
+        }
+
+
         public Flights GetFlights(string src, string dst, string date)
         {
             Console.WriteLine("PIHO.....................................................");
